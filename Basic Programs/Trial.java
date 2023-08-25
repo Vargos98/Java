3868eabdf7b202e1406afe7f286af62475c0bae4
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-25 18:04:11 +0530

    Will start fresh

--- Basic Programs/Trial.java
+++
@@ -1,8 +0,0 @@
-public class Trial {
-    public static void main(String[] args){
-       // String txt ="We are so called \"Vikings\" from the north.";
-        //System.out.println(txt);
-      //  System.out.println(Math.sqrt(64));
-        System.out.println(10>11);
-    }
-}
