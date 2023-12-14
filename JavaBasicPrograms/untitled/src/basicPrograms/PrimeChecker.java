2e7d778494d764f63e9827a09da01133f8faf206
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-14 10:11:12 +0530

    Updated Intellij and Created a primeChecker class

---
+++ JavaBasicPrograms/untitled/src/basicPrograms/PrimeChecker.java
@@ -0,0 +1,25 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please enter the number : ");
+        int n  = sc.nextInt();
+        int c = 2;
+        while (c<n)
+        {
+            if(n%c == 0){
+                System.out.println("Not prime");
+                return;
+
+            }else{
+                System.out.println("Prime");
+                return;
+            }
+
+        }
+        c = c+1;
+    }
+}
