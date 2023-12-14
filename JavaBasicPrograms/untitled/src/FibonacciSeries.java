b1840b8fc1dd4e89edc5b6656db675201f09ec63
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-14 09:41:37 +0530

    Java Codes and Logic for DSA

--- JavaBasicPrograms/untitled/src/FibonacciSeries.java
+++ JavaBasicPrograms/untitled/src/FibonacciSeries.java
@@ -1,23 +1,24 @@
-package BasicPrograms;
-
 import java.util.Scanner;
 
 public class FibonacciSeries {
+    public FibonacciSeries() {
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the Nth value of fibonacci series : ");
         int n = sc.nextInt();
-        int p=0;
-        int i=1;
+        int p = 0;
+        int i = 1;
         int count = 2;
-        while(count<n){
+
+        while(count < n) {
             int temp = i;
-            i = i+p;
-            p= temp;
-            count++;
+            i += p;
+            p = temp;
+            ++count;
             System.out.println(i);
         }
-
 
     }
 }
