07db4191f98b5b3bb17762e5173137445600da16
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-17 11:43:12 +0530

    Update ArrayTrial.java

--- JavaBasicPrograms/untitled/src/DSA/Arrays/ArrayTrial.java
+++ JavaBasicPrograms/untitled/src/DSA/Arrays/ArrayTrial.java
@@ -1,5 +1,6 @@
 package DSA.Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayTrial {
@@ -7,22 +8,28 @@ public class ArrayTrial {
 
         // create an array
         Scanner sc =  new Scanner(System.in);
-        System.out.print("please enter the size of the array : ");
-        int n = sc.nextInt();
-        int[] age = new int[n];
+//        System.out.print("please enter the size of the array : ");
+//        int n = sc.nextInt();
+//        int[] age = new int[n];
+//
+//        for(int i =0;i< n;i++){
+//            age[i] = sc.nextInt();
+//        }
 
-        for(int i =0;i< n;i++){
-            age[i] = sc.nextInt();
-        }
-
-        System.out.println("the entered elements are : ");
+//        System.out.println("the entered elements are : ");
 //        for(int i =0;i < n;i++){
 //            System.out.print(age[i]+" ");
 //        }
-
+//        System.out.println(Arrays.toString(age));
         /// for-each loop
-        for(int i : age){
-            System.out.print(i+" ");
+//        for(int i : age){
+//            System.out.print(i+" ");
+        String[] str = new String[5];
+        for(int i =0;i<str.length;i++){
+            str[i] = sc.next();
         }
+
+        System.out.println(Arrays.toString(str));
+//        }
     }
 }
