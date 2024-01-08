2c5c6c8239b9dac49a724e33d9d1c9c3de9a79e6
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-08 11:22:14 +0530

    Create patternNumTwo.java

---
+++ master/src/JavaBasicPrograms/src/patterns/patternNumTwo.java
@@ -0,0 +1,13 @@
+package JavaBasicPrograms.src.patterns;
+
+public class patternNumTwo {
+    public static void main(String[] args) {
+        int n =5, m =5;
+        for(int i=1; i<=n;i++ ){
+            for(int j =1;j<=n-i+1;j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
