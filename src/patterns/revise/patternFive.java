eb6222dfe5430c8d712e59e13e0b8cc575ba377d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-11 12:31:10 +0530

    Updated patternFive

--- src/patterns/revise/patternFive.java
+++ src/patterns/revise/patternFive.java
@@ -5,10 +5,10 @@ public class patternFive {
         int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print(" * ");
+                System.out.print("* ");
 
             }
             System.out.println();
