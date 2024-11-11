edd5401c24b150c52d60503dc9679d6772d52b40
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-11 10:37:21 +0530

    update pattern 2

--- src/patterns/revise/patternTwo.java
+++ src/patterns/revise/patternTwo.java
@@ -4,13 +4,12 @@ public class patternTwo {
     public static void main(String[] args) {
         int n=5;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
+       for(int i = 1; i<=n;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print(" * ");
+           }
+           System.out.println();
+       }
     }
 }
 // *
