c5293cadfe3a2bece5f4c1434b2bd703d506c739
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-11 10:53:36 +0530

    updated patternFour

--- src/patterns/revise/patternTwo.java
+++ src/patterns/revise/patternTwo.java
@@ -2,7 +2,7 @@ package patterns.revise;
 
 public class patternTwo {
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
 
        for(int i = 1; i<=n;i++){
            for(int j=1;j<=i;j++){
@@ -10,6 +10,35 @@ public class patternTwo {
            }
            System.out.println();
        }
+        System.out.println();
+
+        for(int i = 1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" " + i);
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        char ch = 'A';
+        for(int i = 1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" " +ch);
+                ch++;
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+        char character = 'A';
+        for(int i = 1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" " +character);
+
+            }
+            character++;
+            System.out.println();
+        }
     }
 }
 // *
