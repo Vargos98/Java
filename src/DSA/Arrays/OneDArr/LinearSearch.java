ec5631bef47bbefb53f6b7a67d973986cae66cc6
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-13 11:46:42 +0530

    Completed Linear search

--- src/DSA/Arrays/OneDArr/LinearSearch.java
+++ src/DSA/Arrays/OneDArr/LinearSearch.java
@@ -9,7 +9,7 @@ public class LinearSearch {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the number you want to search : ");
         int n = sc.nextInt();
-        int[] arr = new int[]{12, 13, 98, 67, 72, 100};
+        int arr[] = new int[]{12, 13, 98, 67, 72, 100};
 
         for(int i = 0; i < arr.length; ++i) {
             if (arr[i] == n) {
--- src/DSA/Arrays/OneDArr/SearchInRange.java
+++ src/DSA/Arrays/OneDArr/SearchInRange.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class SearchInRange {
 
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[]{12, 34, 68, 72, 99, 100, 102, 11, 10, 9, 8, 18, 19, 22, 21, 300, 52, 51, 67, 69, 10};
@@ -17,19 +16,19 @@ public class SearchInRange {
         System.out.println(search(arr, start, end, target));
     }
 
-    static int search(int[] arr, int start, int end, int target) {
+    static int search(int arr[], int start, int end, int target) {
         if (arr.length == 0) {
             return -1;
         } else {
-            for(int i = start; i <= end; ++i) {
+            for (int i = start; i <= end; ++i) {
                 if (arr[i] == target) {
-                    System.out.println("Found at : " + i);
-                    return 0;
+                    System.out.println("Found at index: " + i);
+                    return i;  // Return the index where the target was found
                 }
             }
 
             System.out.println("Not Found");
-            return -1;
+            return -1;  // Return -1 if target is not found in the range
         }
     }
 }
