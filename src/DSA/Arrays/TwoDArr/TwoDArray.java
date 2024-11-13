b871d9800a54bd6f283c780b4efa2553aad2d662
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-13 13:09:36 +0530

    Updated TwoDArray

--- src/DSA/Arrays/TwoDArr/TwoDArray.java
+++ src/DSA/Arrays/TwoDArr/TwoDArray.java
@@ -6,36 +6,27 @@ public class TwoDArray {
 
 
     public static void main(String[] args) {
-        System.out.print("Enter 2D array size : ");
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        int columns = sc.nextInt();
-        System.out.println("Enter array elements : ");
-        int[][] twoD = new int[rows][columns];
+        int twoD[][] = new int[5][5];
+        int  k =0;
 
-        int j;
-        for(int i = 0; i < rows; ++i) {
-            for(j = 0; j < columns; ++j) {
-                twoD[i][j] = sc.nextInt();
+        for(int i =0; i<5;i++){
+            for(int j = 0 ;j<5;j++)
+            {
+                twoD[i][j] = k;
+                k++;
             }
         }
 
-        System.out.print("\nData you entered : \n");
-        int[][] var13 = twoD;
-        j = twoD.length;
+        for(int i =0; i<5;i++){
+            for(int j = 0 ;j<5;j++)
+            {
+                System.out.print(twoD[i][j]+" ");
 
-        for(int var7 = 0; var7 < j; ++var7) {
-            int[] x = var13[var7];
-            int[] var9 = x;
-            int var10 = x.length;
-
-            for(int var11 = 0; var11 < var10; ++var11) {
-                int y = var9[var11];
-                System.out.print("" + y + "  ");
             }
-
             System.out.println();
         }
+
+
 
     }
 }
