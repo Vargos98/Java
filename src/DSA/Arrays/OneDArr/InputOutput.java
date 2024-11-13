4f69a405fb6a1f2e2bab96022d3f660854e6f3d3
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-13 11:38:10 +0530

    Update InputOutput.java

--- src/DSA/Arrays/OneDArr/InputOutput.java
+++ src/DSA/Arrays/OneDArr/InputOutput.java
@@ -7,12 +7,12 @@ public class InputOutput {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         String[] names = new String[size];
-
+// for inputting the entered names
         int i;
         for(i = 0; i < size; ++i) {
             names[i] = sc.next();
         }
-
+//        For printing the entered names
         for(i = 0; i < size; ++i) {
             System.out.println(names[i]);
         }
