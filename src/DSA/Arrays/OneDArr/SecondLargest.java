3cbf1aab312a6541a19ed0e7e691faba17a64cbe
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-15 11:29:12 +0530

    Created SecondLargest Element in array
    2 methods :
    1st : assigning 1st and 2nd element of the array. and comparing them.
    2nd: sorting the array and then printing the second last element

---
+++ src/DSA/Arrays/OneDArr/SecondLargest.java
@@ -0,0 +1,33 @@
+package DSA.Arrays.OneDArr;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        int first = Math.max(arr[0], arr[1]);
+        int second = Math.min(arr[0], arr[1]);
+        for(int i=0 ;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+
+            if(arr[i]>first)
+            {
+                second = first;
+                first= arr[i];
+            }else if(arr[i]>second)
+            {
+                second = arr[i];
+            }
+
+        }
+//        Arrays.sort(arr);
+//        second = arr[n-2];
+
+        System.out.println("Second largest element : "+second);
+    }
+}
