e2be66f3efec73b2b4f81ed12f7253ff59f1a7ac
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-16 10:18:20 +0530

    Made the code more simpler to understand

--- src/DSA/Arrays/OneDArr/KthRotationArray.java
+++ src/DSA/Arrays/OneDArr/KthRotationArray.java
@@ -4,20 +4,34 @@ import java.util.Arrays;
 
 public class KthRotationArray {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6};
+        // The array we want to rotate
+        int numbers[] = {1, 2, 3, 4, 5, 6};
 
-        int n = arr.length;
-        int k = 3;
-        k = k%n;  // this method is applied to prevent relooping.
-        while(k-- > 0){
-            int temp = arr[0];
+        // Find out how many times we need to rotate
+        int totalNumbers = numbers.length;
+        int rotations = 3;
 
-            for (int i = 0; i < n - 1; i++) {
-                arr[i] = arr[i + 1];
+        // Prevent unnecessary extra rotations
+        rotations = rotations % totalNumbers;
+
+        // Rotate the array one step at a time
+        while (rotations > 0) {
+            // Remember the first number
+            int firstNumber = numbers[0];
+
+            // Shift all numbers one step to the left
+            for (int i = 0; i < totalNumbers - 1; i++) {
+                numbers[i] = numbers[i + 1];
             }
-            arr[n - 1] = temp;
 
+            // Move the first number to the end
+            numbers[totalNumbers - 1] = firstNumber;
+
+            // Decrease the number of rotations left
+            rotations--;
         }
-        System.out.println(Arrays.toString(arr));
+
+        // Print the rotated array
+        System.out.println(Arrays.toString(numbers));
     }
 }
