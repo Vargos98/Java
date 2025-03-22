f414bb5f9e3bcb035bb281bd5ae02755db997c9b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-03-22 10:56:24 +0530

    Update PrintFactors.java

--- src/EndGame/Basics/PrintFactors.java
+++ src/EndGame/Basics/PrintFactors.java
@@ -3,5 +3,9 @@ package EndGame.Basics;
 import java.util.Scanner;
 
 public class PrintFactors {
-   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
 }
