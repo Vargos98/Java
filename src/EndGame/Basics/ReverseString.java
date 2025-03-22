9a71964a72957a06b657face88f6c63862a858f7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-03-22 12:37:07 +0530

    Create ReverseString.java
    created using string builder

---
+++ src/EndGame/Basics/ReverseString.java
@@ -0,0 +1,13 @@
+package EndGame.Basics;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+    }
+}
