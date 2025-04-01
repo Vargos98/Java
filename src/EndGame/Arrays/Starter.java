36b9139b96c86babbf87d3ddd20fd76e24aba40d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-01 09:04:33 +0530

    update

---
+++ src/EndGame/Arrays/Starter.java
@@ -0,0 +1,10 @@
+package EndGame.Arrays;
+
+import java.util.Scanner;
+
+public class Starter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
