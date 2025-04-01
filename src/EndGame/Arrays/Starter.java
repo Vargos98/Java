7336d979916cb0d121633570a2e15b48761bed11
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-01 09:20:39 +0530

    Created first program of array in java

--- src/EndGame/Arrays/Starter.java
+++ src/EndGame/Arrays/Starter.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Starter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int[] arr = new int[20];
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++)
+        {
+            arr[i]= sc.nextInt();
+
+        }
+        System.out.println("The entered array items is : ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
