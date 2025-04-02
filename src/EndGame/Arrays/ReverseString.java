4fd54ce34c7db88e867a7e917f5d6b818eb2e85c
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-02 10:13:38 +0530

    Update ReverseString.java

--- src/EndGame/Arrays/ReverseString.java
+++ src/EndGame/Arrays/ReverseString.java
@@ -3,15 +3,25 @@ package EndGame.Arrays;
 import java.util.Scanner;
 
 public class ReverseString {
-    public static String revStr(String str){
+
+    // Function to reverse a string using StringBuilder
+    public static String revStr(String str) {
         return new StringBuilder(str).reverse().toString();
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your string");
-        String str = sc.nextLine();
-        System.out.println("Your entered string is : "+str);
 
-        System.out.println("Now reversed string is : "+revStr(str));
+        // Prompt user for input
+        System.out.println("Please enter your string:");
+        String str = sc.nextLine();
+
+        // Display the original string
+        System.out.println("Your entered string is: " + str);
+
+        // Call revStr() function and display the reversed string
+        System.out.println("Now reversed string is: " + revStr(str));
+
+        sc.close(); // Close the scanner to prevent resource leaks
     }
 }
