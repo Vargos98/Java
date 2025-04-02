6b6e1881626572783da5cd8e5a0c91275e49db13
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-02 10:10:28 +0530

    Update CountOccur.java

--- src/EndGame/Arrays/CountOccur.java
+++ src/EndGame/Arrays/CountOccur.java
@@ -3,33 +3,44 @@ package EndGame.Arrays;
 import java.util.Scanner;
 
 public class CountOccur {
-    public static int Occur(int[] arr, int num){
-        int count=0;
-        int i =0;
-        while(i<arr.length){
-            if(arr[i]==num){
+
+    // Function to count the occurrences of a given number in the array
+    public static int Occur(int[] arr, int num) {
+        int count = 0; // Counter for occurrences
+
+        int i = 0;
+        while (i < arr.length) {
+            // If the current element matches the target number, increment the counter
+            if (arr[i] == num) {
                 count++;
             }
-            i++;
+            i++; // Move to the next element
         }
-        return count;
-
+        return count; // Return the total count
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Ask user for the size of the array
         System.out.print("Please enter the size of the array: ");
         int n = sc.nextInt();
 
-        int[] arr = new int[n]; // ✅ Dynamic array based on input size
+        int[] arr = new int[n]; // ✅ Create an array of size 'n'
 
+        // Taking array input from user
         System.out.println("Enter " + n + " elements:");
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Please enter the num you want to count : ");
+
+        // Ask user for the number they want to count occurrences of
+        System.out.print("Please enter the number you want to count: ");
         int num = sc.nextInt();
-        ;
-        System.out.println("The Count for " +num+" is :"  +Occur(arr, num));
+
+        // Display the count of the given number
+        System.out.println("The count for " + num + " is: " + Occur(arr, num));
+
+        sc.close(); // Close the scanner to prevent resource leaks
     }
 }
