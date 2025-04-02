cd400325db0b86cc6057079e3a6808e9cff7cb70
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-02 09:49:59 +0530

    update reverse string

--- src/EndGame/Arrays/ReverseString.java
+++ src/EndGame/Arrays/ReverseString.java
@@ -10,7 +10,7 @@ public class ReverseString {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your string");
         String str = sc.nextLine();
-        System.out.println("Ypur entered string is : "+str);
+        System.out.println("Your entered string is : "+str);
 
         System.out.println("Now reversed string is : "+revStr(str));
     }
