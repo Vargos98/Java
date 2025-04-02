6a86f4375577acb255230a5e3e39de85ae28df08
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-02 10:09:56 +0530

    Update CheckSorted.java

--- src/EndGame/Arrays/CheckSorted.java
+++ src/EndGame/Arrays/CheckSorted.java
@@ -3,28 +3,37 @@ package EndGame.Arrays;
 import java.util.Scanner;
 
 public class CheckSorted {
-    public static boolean SortCheck(int[] nums){
 
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]>nums[i])
-            {
+    // Function to check if the given array is sorted in ascending order
+    public static boolean SortCheck(int[] nums) {
+        // Iterate through the array from the second element to the last
+        for (int i = 1; i < nums.length; i++) {
+            // If the previous element is greater than the current one, the array is not sorted
+            if (nums[i - 1] > nums[i]) {
                 return false;
             }
         }
-        return true;
+        return true; // If no such case is found, the array is sorted
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Ask user for the size of the array
         System.out.print("Please enter the size of the array: ");
         int n = sc.nextInt();
 
-        int[] nums = new int[n]; // ✅ Dynamic array based on input size
+        int[] nums = new int[n]; // ✅ Create an array of size 'n'
 
+        // Taking array input from user
         System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
 
-        System.out.println("The result is : "+SortCheck(nums));
+        // Display whether the array is sorted or not
+        System.out.println("Is the array sorted? " + SortCheck(nums));
+
+        sc.close(); // Close the scanner to prevent resource leaks
     }
 }
