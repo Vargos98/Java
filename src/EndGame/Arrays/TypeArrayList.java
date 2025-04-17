c231ccfa1f4b5ef22eae47d379a16a9943efb6c4
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-04-17 09:54:19 +0530

    Update

---
+++ src/EndGame/Arrays/TypeArrayList.java
@@ -0,0 +1,37 @@
+package EndGame.Arrays;
+
+import java.util.ArrayList;
+
+public class TypeArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+        list.add(200);
+        list.add(201);
+        list.add(58);
+        list.add(64);
+
+        System.out.println(list);
+
+    }
+}
